7dfc5fca06c6bbb29e048821f5b1ed25c3dccb92
Gudala Nandini <gudalanandini2939@gmail.com>
2026-06-13 15:49:54 +0530

    Trigger redeploy

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("InsightBoard API Running...");
+  res.send("InsightBoard API Running...!");
 });
 
 app.use("/api/auth", authRoutes);
